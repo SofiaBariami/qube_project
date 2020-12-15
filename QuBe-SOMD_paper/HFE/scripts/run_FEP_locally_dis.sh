52ac497b7ff67edfb7d65384e0c7e455c61b1d02
Sofia Bariami <44237397+SofiaBariami@users.noreply.github.com>
2020-12-15 17:15:36 +0000

    Update and rename run_FEP_locally.sh to run_FEP_locally_dis.sh

--- QuBe-SOMD_paper/HFE/scripts/run_FEP_locally_dis.sh
+++ QuBe-SOMD_paper/HFE/scripts/run_FEP_locally_dis.sh
@@ -4,6 +4,6 @@ for i in $(seq 0.00 0.10 1.00)
 do
 mkdir lambda-$i
 cd lambda-$i
-~/sire.app/bin/somd-freenrg -C  ../../input/sim.cfg -d 0 -l $i -p OpenCL
+~/sire.app/bin/somd-freenrg -C  ../../input/sim_dis.cfg -d 0 -l $i -p OpenCL
 cd ../
 done
